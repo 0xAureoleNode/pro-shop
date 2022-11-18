4db52ee1a72714269ef8a572d797427f13772702
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-18 16:09:58 +0800

    Fetching Products From The DataBase

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import products from './data/products.js';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -11,16 +13,7 @@ app.get('/', (req, res) => {
   res.send('API is running..........');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
-connectDB();
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 

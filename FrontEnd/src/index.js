5fc7f61948b5e0318959d26e63c077ffe282e685
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-22 20:11:18 +0800

    Added Cart reducers Added to Cart Action

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+const root = document.getElementById('root');
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    root
 );
--- FrontEnd/src/store.js
+++ FrontEnd/src/store.js
@@ -2,22 +2,30 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/productListReducers';
-import { productDetailsReducer } from './reducers/productDetailsReducers';
+import { productListReducer } from './reducers/productReducers/productListReducers';
+import { productDetailsReducer } from './reducers/productReducers/productDetailsReducers';
+import { cartReducer } from './reducers/cartReducers/cartReducers';
 
 const reducer = combineReducers({
-  productList: productListReducer,
-  productDetails: productDetailsReducer,
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemFromStorage = localStorage.getItem('cartItems')
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    : [];
+
+const initialState = {
+    cart: { cartItems: cartItemFromStorage },
+};
 
 const middleware = [thunk];
 
 const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;

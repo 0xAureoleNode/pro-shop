b67f50228375439cf601f4fb16d634008f0d6867
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-22 15:10:25 +0800

    1.added Quantity Select function 2.added to cart button

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -4,20 +4,22 @@ import Footer from './components/Footer';
 import Container from 'react-bootstrap/Container';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 const App = () => {
-  return (
-    <Router>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <Route path="/" component={HomeScreen} exact />
-          <Route path="/product/:id" component={ProductScreen} />
-        </Container>
-      </main>
-      <Footer />
-    </Router>
-  );
+    return (
+        <Router>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <Route path="/" component={HomeScreen} exact />
+                    <Route path="/product/:id" component={ProductScreen} />
+                    <Route path="/cart/:id?" component={CartScreen} />
+                </Container>
+            </main>
+            <Footer />
+        </Router>
+    );
 };
 
 export default App;
